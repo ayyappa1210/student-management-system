8cf25b04eea24e30f0315ede1789e66375234287
ayyappa1210 <ayyappaponnapoola143@gmail.com>
2025-06-26 23:37:09 +0530

    added logging to create student api

--- src/main/java/com/demo/sms/controller/StudentController.java
+++ src/main/java/com/demo/sms/controller/StudentController.java
@@ -2,7 +2,10 @@ package com.demo.sms.controller;
 
 import com.demo.sms.dto.StudentRequest;
 import com.demo.sms.dto.StudentResponse;
+import com.demo.sms.exception.EmailAlreadyExistsException;
 import com.demo.sms.service.StudentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/students")
 public class StudentController {
 
+    private static final Logger logger =LoggerFactory.getLogger(StudentController.class);
+
     private final StudentService studentService;
 
     public StudentController(StudentService studentService) {
@@ -22,6 +27,18 @@ public class StudentController {
 
     @PostMapping
     public ResponseEntity<StudentResponse> createStudent(@RequestBody StudentRequest studentRequest) {
-        return new ResponseEntity<>(studentService.createStudent(studentRequest), HttpStatus.CREATED);
+        logger.info("Received request to create student: {}",studentRequest.getEmail());
+        try {
+            StudentResponse studentResponse = studentService.createStudent(studentRequest);
+            logger.info("Successfully created Student with ID: {}",studentResponse.getId());
+            return new ResponseEntity<>(studentResponse, HttpStatus.CREATED);
+        } catch (EmailAlreadyExistsException e) {
+            logger.warn("Attempt to create student with existing email {}: {}", studentRequest.getEmail(), e.getMessage());
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(new StudentResponse(null,null,e.getMessage(),null,null,null));
+        }catch (Exception e) {
+            logger.error("Error creating student for email {}: {}", studentRequest.getEmail(), e.getMessage(), e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new StudentResponse(null,null,"An unexpected error occured",null,null,null));
+        }
+
     }
 }
--- src/main/java/com/demo/sms/service/StudentService.java
+++ src/main/java/com/demo/sms/service/StudentService.java
@@ -2,8 +2,9 @@ package com.demo.sms.service;
 
 import com.demo.sms.dto.StudentRequest;
 import com.demo.sms.dto.StudentResponse;
+import com.demo.sms.exception.EmailAlreadyExistsException;
 
 public interface StudentService {
 
-    StudentResponse createStudent(StudentRequest studentRequest);
+    StudentResponse createStudent(StudentRequest studentRequest) throws EmailAlreadyExistsException;
 }
